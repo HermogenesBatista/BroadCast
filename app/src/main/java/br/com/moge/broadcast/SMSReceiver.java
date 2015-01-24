f97b7c67160aee69c76eef78b77c24f4eeaaacf1
Hermogenes Batista da Silva Filho <exemogenes@gmail.com>
2015-01-24 18:22:20 -0300

    Criando o Repositorio222

---
+++ app/src/main/java/br/com/moge/broadcast/SMSReceiver.java
@@ -0,0 +1,15 @@
+package br.com.moge.broadcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by cd-107 on 24/01/2015.
+ */
+public class SMSReceiver extends Intent {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
