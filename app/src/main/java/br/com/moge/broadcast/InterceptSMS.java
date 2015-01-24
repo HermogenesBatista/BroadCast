11d50059893fe4f544ad8613e78b5c61e13673f2
Hermogenes Batista da Silva Filho <exemogenes@gmail.com>
2015-01-24 18:38:30 -0300

    teste
    teste

---
+++ app/src/main/java/br/com/moge/broadcast/InterceptSMS.java
@@ -0,0 +1,11 @@
+package br.com.moge.broadcast;
+
+/**
+ * Created by cd-107 on 24/01/2015.
+ */
+public class InterceptSMS implements Runnable {
+    @Override
+    public void run() {
+
+    }
+}
--- app/src/main/java/br/com/moge/broadcast/SMSReceiver.java
+++ app/src/main/java/br/com/moge/broadcast/SMSReceiver.java
@@ -11,6 +11,6 @@ public class SMSReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        new Thread();
+        Thread t = new Thread();
     }
 }
