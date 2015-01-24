a682bc7868f888fe87e9d778731680b69d842bfc
Hermogenes Batista da Silva Filho <exemogenes@gmail.com>
2015-01-24 19:04:43 -0300

    Recebendo SMS e notificando! :)

--- app/src/main/java/br/com/moge/broadcast/SMSReceiver.java
+++ app/src/main/java/br/com/moge/broadcast/SMSReceiver.java
@@ -12,34 +12,26 @@ import android.widget.Toast;
 public class SMSReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, final Intent intent) {
-        Thread t = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                Bundle bundle = intent.getExtras();
-                SmsMessage[] msgs = null;
-                String str = "";
-                String address = "";
+        Bundle bundle = intent.getExtras();
+        SmsMessage[] msgs = null;
+        String str = "";
+        String address = "";
 
-                if (bundle != null) {
-                    Object[] pdus = (Object[]) bundle.get("pdus");
-                    msgs = new SmsMessage[pdus.length];
-                    for (int i = 0; i < msgs.length; i++) {
-                        msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
-                        str += "SMS from " + msgs[i].getOriginatingAddress();
-                        str += " :";
-                        str += msgs[i].getMessageBody().toString();
-                        str += "\n";
-                        address = msgs[i].getOriginatingAddress();
-                        Toast.makeText(context, str, Toast.LENGTH_LONG).show();
-                    }
+        if (bundle != null) {
+            Object[] pdus = (Object[]) bundle.get("pdus");
+            msgs = new SmsMessage[pdus.length];
+            for (int i = 0; i < msgs.length; i++) {
+                msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                str += "SMS from " + msgs[i].getOriginatingAddress();
+                str += " :";
+                str += msgs[i].getMessageBody().toString();
+                str += "\n";
 
-                }
+
+                address = msgs[i].getOriginatingAddress();
+                Toast.makeText(context, str, Toast.LENGTH_LONG).show();
             }
+
         }
-
-        );
-
-
-        t.start();
     }
 }
