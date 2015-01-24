a7465bdf384265998938044fc8e9fb6700ee7b0a
Hermogenes Batista da Silva Filho <exemogenes@gmail.com>
2015-01-24 18:42:36 -0300

    Mudança na Classe SMSReceiver - Implementando o OnReceiver

--- app/src/main/java/br/com/moge/broadcast/SMSReceiver.java
+++ app/src/main/java/br/com/moge/broadcast/SMSReceiver.java
@@ -3,14 +3,43 @@ package br.com.moge.broadcast;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
 /**
  * Created by cd-107 on 24/01/2015.
  */
 public class SMSReceiver extends BroadcastReceiver {
     @Override
-    public void onReceive(Context context, Intent intent) {
+    public void onReceive(final Context context, final Intent intent) {
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Bundle bundle = intent.getExtras();
+                SmsMessage[] msgs = null;
+                String str = "";
+                String address = "";
 
-        Thread t = new Thread();
+                if (bundle != null) {
+                    Object[] pdus = (Object[]) bundle.get("pdus");
+                    msgs = new SmsMessage[pdus.length];
+                    for (int i = 0; i < msgs.length; i++) {
+                        msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                        str += "SMS from " + msgs[i].getOriginatingAddress();
+                        str += " :";
+                        str += msgs[i].getMessageBody().toString();
+                        str += "\n";
+                        address = msgs[i].getOriginatingAddress();
+                        Toast.makeText(context, str, Toast.LENGTH_LONG).show();
+                    }
+
+                }
+            }
+        }
+
+        );
+
+
+        t.start();
     }
 }
